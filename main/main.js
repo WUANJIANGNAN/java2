f34c3f0016955fd2f342647da1d54e50b7fb3f97
WUAN <291829614@qq.com>
2019-05-28 22:41:16 +0800

    fix

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num)
+{
+	var i;
+	var j;
+	var text;
+	var up = [];
+	var mid = [];
+	var down = [];
+	for(i = 0, i < num.length, i++)
+	{
+		j = 0;
+		while(j != Number(i))
+			j++;
+		text += up[j - 1];
+	}
+	text = text + "\n";
+	for(i = 0, i < num.length, i++)
+	{
+		j = 0;
+		while(j != Number(i))
+			j++;
+		text += up[j - 1];
+	}
+	text = text + "\n";
+	for(i = 0, i < num.length, i++)
+	{
+		j = 0;
+		while(j != Number(i))
+			j++;
+		text += up[j - 1];
+	}
+	text = text + "\n";
+	console.log(text);
+	return text;
 };
